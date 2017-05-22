342af896a5a73f7f0c8b57c7bf80334d22c4ed8d
tongtn <versnoontt02@gmail.com>
2017-05-22 08:26:53 +0800

    去除多余import

--- cy-common/src/main/java/io/jianxun/source/repository/ERPMchkRepository.java
+++ cy-common/src/main/java/io/jianxun/source/repository/ERPMchkRepository.java
@@ -1,9 +1,9 @@
 package io.jianxun.source.repository;
 
-import io.jianxun.source.domain.ERPHuoweizl;
-import io.jianxun.source.domain.ERPMchk;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+
+import io.jianxun.source.domain.ERPMchk;
 
 public interface ERPMchkRepository
 		extends JpaRepository<ERPMchk, String>, QueryDslPredicateExecutor<ERPMchk> {
